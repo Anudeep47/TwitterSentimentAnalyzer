0125e51ea1eeb4afd1d890cad5f0c2a1bbc99725
Anudeep Pala <anudeep479@gmail.com>
2017-10-16 13:39:52 +0530

    deleting tokens

--- demo.py
+++ demo.py
@@ -1,11 +1,11 @@
 import tweepy
 from textblob import TextBlob
 
-consumer_key= 'gotBtAwotLo3zkjIWTV0CGwzk'
-consumer_secret_key= 'fA0CJcyhDe3kWoClrSL88aD9nR7aPHcgfocew6xUfX7xts8MhG'
+consumer_key= 'CONSUMER_KEY_HERE'
+consumer_secret_key= 'CONSUMER_SECRET_HERE'
 
-access_token= '775765231398039553-ZWz4yPhT9N99u3YqzqlnoJZXB36rs8r'
-access_token_secret= 'RFqLJajEp6jjYrbAbyK429JBpyJIu8qD5ONdaoUEM4jEP'
+access_token= 'ACCESS_TOKEN_HERE'
+access_token_secret= 'ACCESS_TOKEN_SECRET_HERE'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret_key)
 auth.set_access_token(access_token, access_token_secret)
